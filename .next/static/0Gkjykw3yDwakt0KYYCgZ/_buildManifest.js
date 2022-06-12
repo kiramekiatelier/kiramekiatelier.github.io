09367388bce7a7d525c94340957eca7695a3a2f7
yuuukun <58155530+yuuukun@users.noreply.github.com>
2022-06-12 22:16:44 +0700

    yeet

---
+++ .next/static/0Gkjykw3yDwakt0KYYCgZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-212c0d687bf26ce5.js","static/chunks/675-c58e00aa159d32c4.js","static/chunks/pages/index-7581833685a71448.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
